e71dc6794359efd63acf081f7a16ef87f3532f0e
ranidspace <ranidspace@proton.me>
2024-06-20 19:10:32 -0400

    redo of BC5, references aboood40091/BNTX-Editor again, rewrite later.

--- bfres/BNTX/pixelfmt/bc/bc5.py
+++ bfres/BNTX/pixelfmt/bc/bc5.py
@@ -12,8 +12,8 @@ class BC5(TextureFormat, BCn):
         decode   = self.decodePixel
         bpp      = self.bytesPerPixel
         data     = tex.data
-        width    = int((tex.width  + 3) / 4)
-        height   = int((tex.height + 3) / 4)
+        width    = (tex.width  + 3) // 4
+        height   = (tex.height + 3) // 4
         pixels   = bytearray(width * height * 64)
         swizzle  = tex.swizzle.getOffset
         is_snorm = tex.fmt_dtype.name == 'SNorm'
@@ -21,52 +21,102 @@ class BC5(TextureFormat, BCn):
         for y in range(height):
             for x in range(width):
                 offs    = swizzle(x, y)
-                red     = self.calcAlpha(data[offs : offs+2], tex)
-                redCh   = struct.unpack('Q', red)[0]
-                green   = self.calcAlpha(data[offs+8 : offs+10], tex)
-                greenCh = struct.unpack('Q', green)[0]
+                redbits, ralph = self.calcAlpha(data[offs : offs+8])
+                grnbits, galph = self.calcAlpha(data[offs+8 : offs+16])
 
-                toffs = 0
                 if is_snorm:
                     for ty in range(4):
                         for tx in range(4):
-                            shift = ty * 12 + tx * 3
                             out = (x*4 + tx + (y*4 + ty)*width*4)*4
-                            r   = red  [(redCh   >> shift) & 7] + 0x80
-                            g   = green[(greenCh >> shift) & 7] + 0x80
-                            nx = (r / 255.0) * 2 - 1
-                            ny = (g / 255.0) * 2 - 1
-                            nz = math.sqrt(1 - (nx*nx + ny*ny))
-                            pixels[out : out+4] = (
-                                clamp((nz+1) * 0.5),
-                                clamp((ny+1) * 0.5),
-                                clamp((nx+1) * 0.5),
-                                0xFF)
-                            toffs += 4
+                            r = self.decodeAlphaSigned(redbits, ralph)
+                            g = self.decodeAlphaSigned(grnbits, galph)
+                            pixels[out : out+4] = (0, g, r, 255)
+                            redbits >>= 3
+                            grnbits >>= 3
+                            continue
                 else:
                     for ty in range(4):
                         for tx in range(4):
-                            shift = ty * 12 + tx * 3
                             out = (x*4 + tx + (y*4 + ty)*width*4)*4
-                            r   = red  [(redCh   >> shift) & 7]
-                            g   = green[(greenCh >> shift) & 7]
-                            pixels[out : out+4] = (r, r, r, g)
-                            toffs += 4
+                            r = self.decodeAlpha(redbits, ralph)
+                            g = self.decodeAlpha(grnbits, galph)
+                            pixels[out : out+4] = (0, g, r, 255)
+                            redbits >>= 3
+                            grnbits >>= 3
 
         return pixels, self.depth
 
 
-    def calcAlpha(self, alpha, tex):
-        if tex.fmt_dtype.name != 'SNorm':
-            return super().calcAlpha(alpha)
+    def calcAlpha(self, alpha):
+            alpha0 = alpha[0]
+            alpha1 = alpha[1]
+            bits = (alpha[0] | (alpha[1] << 8 ) |
+            (alpha[2] << 16) | (alpha[3] << 24) |
+            (alpha[4] << 32) | (alpha[5] << 40) |
+            (alpha[6] << 48) | (alpha[7] << 56)) >> 16
+            return bits, (alpha0, alpha1)
+    
+    def decodeAlpha(self, bits, alpha):
+        code = bits & 0x07
+        if code == 0:
+            ACOMP = alpha[0]
+        elif code == 1:
+            ACOMP = alpha[1]
 
-        a0, a1 = alpha[0], alpha[1]
-        d = (a0, a1, 0, 0, 0, 0, 0x80, 0x7F)
-        alpha = bytearray(bytes(d))
-        for i in range(2, 6):
-            # XXX do we need to cast here?
-            if a0 > a1:
-                alpha[i] = int(((8-i) * a0 + (i-1) * a1) / 7)
-            else:
-                alpha[i] = int(((6-i) * a0 + (i-1) * a1) / 7)
-        return alpha
+        elif alpha[0] > alpha[1]:
+            ACOMP = (alpha[0] * (8 - code) + (alpha[1] * (code - 1))) // 7
+
+        elif code < 6:
+            ACOMP = (alpha[0] * (6 - code) + (alpha[1] * (code - 1))) // 5
+
+        elif code == 6:
+            ACOMP = 0
+
+        else:
+            ACOMP = 255
+        return ACOMP
+    
+    def decodeAlphaSigned(self, bits, alpha):
+        code = bits & 0x07
+        if code == 0:
+            ACOMP = alpha[0]
+
+        elif code == 1:
+            ACOMP = alpha[1]
+
+        elif ToSigned8(alpha[0]) > ToSigned8(alpha[1]):
+            ACOMP = ToUnsigned8((ToSigned8(alpha[0]) * (8 - code) + (ToSigned8(alpha[1]) * (code - 1))) // 7)
+
+        elif code < 6:
+            ACOMP = ToUnsigned8((ToSigned8(alpha[0]) * (6 - code) + (ToSigned8(alpha[1]) * (code - 1))) // 5)
+
+        elif code == 6:
+            ACOMP = 0x80
+
+        else:
+            ACOMP = 0x7f
+        return ACOMP
+
+def ToUnsigned8(v):
+    if v > 127:
+        return 127
+
+    elif v < -128:
+        return 128
+
+    elif v < 0:
+        return v + 256
+
+    return v
+
+def ToSigned8(v):
+    if v > 255:
+        return -1
+
+    elif v < 0:
+        return 0
+
+    elif v > 127:
+        return v - 256
+
+    return v
